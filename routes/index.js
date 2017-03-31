873d60a9cc955210da5d08128dfb0c53abe8564c
Matthew Marcos <matthewmarcos94@gmail.com>
2017-03-31 21:25:08 +0800

    nodemon na tayo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send('penge tweets');
 });
 
 module.exports = router;
